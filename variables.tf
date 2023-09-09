b932d18d3d081c66655a392c8019d17dc22731d3
kkprbr <110367019+kkprbr@users.noreply.github.com>
2023-09-09 19:09:36 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "rgname" {
+    type = string
+    description = "used for resource group naming"
+}
+
+variable "location" {
+    type = string
+    description = "used for selecting the location"
+    default = "eastus"
+}
+
+variable "prefix" {
+   type = string
+   description = "used for networking"
+}
+variable "vnet_cidr_prefix" {
+    type = string
+    description = "this variable defined address space for vnet"
+}
+
+variable "subnet1_cidr_prefix" {
+    type = string 
+    description = "this variable defined address space for subnet"
+}
