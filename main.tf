8169c82410bd03267f3d2af88ba7cb244f621c8a
kkprbr <110367019+kkprbr@users.noreply.github.com>
2023-09-09 19:59:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name = "subnet1"
+  name                  = "var.subnet"
   virtual_network_name  = "${azurerm_virtual_network.vnet.name}"
   resource_group_name   = "${azurerm_resource_group.rg.name}" 
   address_prefixes      = ["${var.subnet1_cidr_prefix}"]
